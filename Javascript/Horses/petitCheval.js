6744b20ebcf24396c3a02a1c59237540c80c81d8
Poli <APO3794@viseo.net>
2018-03-15 14:53:36 +0100

    added color scheme

--- Javascript/Horses/petitCheval.js
+++ Javascript/Horses/petitCheval.js
@@ -1,24 +1,24 @@
 
 throwDice = random => Math.floor(Math.random() * 6 + 1);
 
-var player1 = { currentPos: "0", inGame: true, color: "red" }
-var player2 = { currentPos: "0", inGame: true, color: "blue" }
-var player3 = { currentPos: "0", inGame: true, color: "green" }
-var player4 = { currentPos: "0", inGame: true, color: "yellow" }
+var player1 = { currentPos: "0", inGame: true, color: "red", textColor:"#cc0000" }
+var player2 = { currentPos: "0", inGame: true, color: "blue", textColor:"#4d4dff" }
+var player3 = { currentPos: "0", inGame: true, color: "green", textColor:"#00b33c" }
+var player4 = { currentPos: "0", inGame: true, color: "yellow", textColor:"#ffcc00" }
 var players = [player1, player2, player3, player4]
 
 function playRound(player) {
     let result = throwDice();
-    console.log(`Player ${player.color} in position ${player.currentPos} scored a ${result}`)
+    console.log(`%c Player ${player.color} in position ${player.currentPos} scored a ${result}`, `color: ${player.textColor}`)
     if (parseInt(player.currentPos + result) > 56) {
-        console.log('He can\'t move !')
+        console.log('%c He can\'t move !', `color: ${player.textColor}`)
     } else if (parseInt(player.currentPos + result) === 56) {
-        console.log(`Player ${player.color} won and exited the game`)
+        console.log(`%c Player ${player.color} won and exited the game`, `color: ${player.textColor}`)
         player.inGame = false
     }
     else {
         player.currentPos = parseInt(player.currentPos + result)
-        console.log(`Player ${player.color} new position is ${player.currentPos}`)
+        console.log(`%c Player ${player.color} new position is ${player.currentPos}`, `color: ${player.textColor}`)
         moveHorseInSameSpot(player)
     }
 }
@@ -27,20 +27,29 @@ function moveHorseInSameSpot(mainPlayer){
     players.forEach(player => {
         if(player.color !== mainPlayer.color && player.currentPos === mainPlayer.currentPos){
             player.currentPos=0
-            console.log(`Player ${player.color} got kicked out by player ${mainPlayer.color}`)
-
+            console.log(`%c Player ${player.color} got kicked out by player ${mainPlayer.color}`, `color: ${player.textColor}`)
         }
     })
 }
 
 game = () => {
     let playerCount = players.length;
-    while (playerCount > 1) {
+    while (true) {
+        for(let i=0; i<playerCount; ++i){
+            if (playerCount===1){
+                return
+            }
+            playRound(players[i])
+            players = players.filter(player => player.inGame === true)
+            playerCount = players.length
+        }
+        /*
         players.forEach(player => {
             playRound(player)
             players = players.filter(player => player.inGame === true)
             playerCount = players.length
-        })
+        })*/
     }
 }
+
 game()
